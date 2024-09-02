d06b69b85c6a047879ad9bee51273728769f771c
yohanesokta <yohanesoktayt72@gmail.com>
2024-09-02 20:52:12 +0700

    new data

--- script.js
+++ script.js
@@ -1,63 +1,103 @@
-function transformCloudinaryURL(originalURL, width, height, fitType) {
-    let parts = originalURL.split("/upload/");
-    if (parts.length === 2) {
-        let transformedURL = parts[0] + "/upload/w_" + width + ",h_" + height + ",c_" + fitType + "/" + parts[1];
-        return transformedURL;
+function PengubahNomor(no) {
+    if (String(no)[0] == "+") {
+        return no
     } else {
-        console.error("URL Cloudinary tidak valid.");
-        return null;
+        let data = String(no).split('')
+        delete (data[0])
+        data = "+62" + data.join('')
+        return data
     }
 }
 
+function cardComponent(element) {
+    return `<a class="teman-profile" id="teman-profile" href="https://wa.me/${PengubahNomor(element.no)}" target="_blank"">
+    <div class="image">
+        <img src="${element.fotoselfie}"
+            alt="">
+    </div>
+    <div class="right-profile">
+        <h1>${element.nama}</h1>
+        <span>Kelompok ${element.kelompok}</span>
+        <div class="bio-next">
+            <div class="name">
+                <ul>
+                    <li>
+                        NIM
+                    </li>
+                    <li>
+                        Alamat
+                    </li>
+                    <li>
+                        Tgl Lahir
+                    </li>
+                </ul>
+            </div>
+            <div class="field">
+                <ul>
+                    <li>
+                        ${element.nim}
+                    </li>
+                    <li class="alamat">
+                        ${element.alamat}
+                    </li>
+                    <li>
+                        ${element.ttl}
+                    </li>
+                </ul>
+            </div>
+        </div>
 
-async function load() {
-    let data = await fetch('./src/data.json');
-    data = await data.json()
+        <h3 class="telp">${element.no}</h3>
+    </div>
+    <div class="teman-hover"><i class="fa-solid fa-arrow-up-right-from-square"></i></div>
+</a>`
+}
+
+const Teman = (no) => {
+    window.location.href = `https://wa.me/${no}`
+}
+
+
+let globalData;
+function draw(data) {
     let component = ""
     data.forEach(element => {
-        component += `<div class="teman-profile">
-        <div class="image">
-            <img src="${element.fotoselfie}"
-                alt="">
-        </div>
-        <div class="right-profile">
-            <h1>${element.nama}</h1>
-            <span>Kelompok ${element.kelompok}</span>
-            <div class="bio-next">
-                <div class="name">
-                    <ul>
-                        <li>
-                            NIM
-                        </li>
-                        <li>
-                            Alamat
-                        </li>
-                        <li>
-                            Tgl Lahir
-                        </li>
-                    </ul>
-                </div>
-                <div class="field">
-                    <ul>
-                        <li>
-                            ${element.nim}
-                        </li>
-                        <li class="alamat">
-                            ${element.alamat}
-                        </li>
-                        <li>
-                            ${element.ttl}
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <h3 class="telp">${element.no}</h3>
-        </div>
-    </div>`
+        component += cardComponent(element);
     });
 
     document.getElementById("container-kanan").innerHTML += component
 }
 
+
+async function load() {
+    let data = await fetch('./src/data.json');
+    globalData = await data.json()
+    draw(globalData)
+}
+
 load()
+
+
+const form = document.querySelector('form')
+form.addEventListener('submit', ev => {
+    ev.preventDefault()
+    const input = document.getElementById('se');
+    document.getElementById("container-kanan").innerHTML = ""
+    let component = ` <div class="gap"></div>
+    <div class="name-big">
+        <h1>Yohanes Oktanio</h1>
+        <h3><i class="fa-brands fa-ubuntu"></i> UBUNTU / 5</h3>
+    </div>`
+    globalData.forEach(element => {
+
+        if (String(element.nama).toLowerCase().includes(input.value.toLowerCase())) {
+            component += cardComponent(element)
+        }
+    })
+
+    document.getElementById("container-kanan").innerHTML += component
+
+})
+
+
+
